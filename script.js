fa44c34e88aaa7df55456d7c35f4262c0ddcbab1
giangtruong2402 <105619226+giangtruong2402@users.noreply.github.com>
2024-10-20 19:22:54 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+<script>
+    document.querySelector('.card').addEventListener('click', function() {
+        // Ẩn card
+        document.querySelector('.card').style.display = 'none';
+        // Hiện frames
+        document.querySelector('.block').style.display = 'flex';
+    });
+</script>
